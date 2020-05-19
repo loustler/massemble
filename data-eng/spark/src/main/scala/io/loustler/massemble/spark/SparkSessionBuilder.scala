0668402ed241c19b48be8fffec1d368dd94075ca
loustler <dev.loustler@gmail.com>
2020-05-20 00:12:37 +0900

    done

--- data-eng/spark/src/main/scala/io/loustler/massemble/spark/SparkSessionBuilder.scala
+++ data-eng/spark/src/main/scala/io/loustler/massemble/spark/SparkSessionBuilder.scala
@@ -3,5 +3,11 @@ package io.loustler.massemble.spark
 import org.apache.spark.sql.SparkSession
 
 object SparkSessionBuilder {
+
+  /**
+    * With 2 core in Local
+    *
+    * @return
+    */
   def local: SparkSession.Builder = SparkSession.builder.master("local[2]")
 }
