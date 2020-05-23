76778fa9a391ea355aa55d79bfb2ee6f79ba97d7
loustler <dev.loustler@gmail.com>
2020-05-23 11:23:38 +0900

    [DATAENG] Spark. Add Hive example for spark version above 2.0.0

---
+++ data-eng/spark/src/main/scala/io/loustler/massemble/spark/hive/Hive.scala
@@ -0,0 +1,21 @@
+package io.loustler.massemble.spark.hive
+
+import io.loustler.massemble.spark.builder.{ SparkContextBuilder, SparkSessionBuilder }
+import org.apache.spark.sql._
+
+object Hive {
+
+  def main(args: Array[String]): Unit = {
+    // HiveContext deprecated since 2.0.0
+    // Example see https://spark.apache.org/docs/latest/sql-data-sources-hive-tables.html
+    val spark = SparkSessionBuilder.local
+      .appName("Hive")
+      .config("spark.sql.warehouse.dir", "spark-warehouse-path")
+      .enableHiveSupport()
+      .getOrCreate()
+
+    import spark.implicits._
+
+    spark.sql("SELECT * FROM table").show()
+  }
+}
