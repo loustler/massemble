64be5a459d5f0515cc7b7110a957951cd56fb2a6
loustler <dev.loustler@gmail.com>
2020-06-03 23:22:11 +0900

    wip

--- data-eng/spark/src/main/scala/io/loustler/massemble/spark/dataframe/json/JsonDF.scala
+++ data-eng/spark/src/main/scala/io/loustler/massemble/spark/dataframe/json/JsonDF.scala
@@ -1,6 +1,7 @@
 package io.loustler.massemble.spark.dataframe.json
 
 import io.loustler.massemble.spark.builder.SparkSessionBuilder
+import io.loustler.massemble.spark.file.{ FileFormat, FilePath }
 
 object JsonDF {
 
@@ -9,6 +10,13 @@ object JsonDF {
       .appName("JsonDF")
       .getOrCreate()
 
-    val df = spark.read.json("")
+    val df = spark.read
+      .option("multiline", "true")
+      .json(FilePath.path(FileFormat.JSON, "dummy.json"))
+      .cache()
+
+    df.show()
+
+    df.unpersist()
   }
 }
