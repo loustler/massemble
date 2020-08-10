6401c806c6b8b1ce5d1d57d8fe87bb4abea138b2
loustler <dev.loustler@gmail.com>
2020-08-10 22:32:54 +0900

    Bump dependencies

--- functional-programming/zio/project/Version.scala
+++ functional-programming/zio/project/Version.scala
@@ -1,11 +1,11 @@
 object V {
-  val zio = "1.0.0-RC20"
+  val zio = "1.0.0"
 
-  val `zio-interop-cats` = "2.1.3.0-RC15"
+  val `zio-interop-cats` = "2.1.4.0"
 
-  val `zio-config` = "1.0.0-RC19"
+  val `zio-config` = "1.0.0-RC26"
 
-  val `zio-nio` = "1.0.0-RC7"
+  val `zio-nio` = "1.0.0-RC9"
 
-  val `zio-logging` = "0.2.9"
+  val `zio-logging` = "0.4.0"
 }
